92bc5a80e50fe27c34bb7fa7b45c0254f84c3917
Rkislam3390990 <ri3390990@gmail.com>
2024-09-18 21:03:46 +0600

    first commit

---
+++ js/Script.js
@@ -0,0 +1,10 @@
+$('.sliderPart').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:'<i class="fa-solid fa-angle-left prev"></i>',
+    nextArrow:'<i class="fa-solid fa-angle-right next"></i>',
+    dots:true,
+  });
+ 
